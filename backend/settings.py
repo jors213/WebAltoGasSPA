1fce8a007859849212282ac6743ea1f420849c82
Jorge <jcanchig1@gmail.com>
2026-01-05 23:29:50 -0300

    Corrigiendo Allowed Host y CSRF para Railway

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-2aa7b.up.railway.app'
+]
 
 
 # Application definition
