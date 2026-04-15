888df6f95b09599c1371f917725222714add26c0
Jorge <jcanchig1@gmail.com>
2026-04-15 18:21:12 -0400

    SearchChanges 2

--- backend/urls.py
+++ backend/urls.py
@@ -1,24 +1,29 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from core.views import home
+from django.http import HttpResponse
+
+robots_txt = """User-agent: *
+Disallow: /admin/
+Allow: /
+Sitemap: https://www.altogasspa.cl/sitemap.xml
+"""
+
+sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://www.altogasspa.cl/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>
+"""
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+
+    path('robots.txt', lambda request: HttpResponse(robots_txt, content_type='text/plain')),
+    path('sitemap.xml', lambda request: HttpResponse(sitemap_xml, content_type='application/xml')),
 ]
